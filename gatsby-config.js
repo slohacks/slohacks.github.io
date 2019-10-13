f5a63519f1ad4c1356fc5611f8c1c052e0748607
Jumer-Caragay <jumer.caragay@gmail.com>
2019-10-13 15:40:48 -0700

    feat(all): add initial project files

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: 'SLO Hacks',
+    description: 'Event website for SLO Hacks 2020 hackathon.',
+    author: '@slohacks',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+  ],
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const IndexPage = () => (
+  <WrapperContainer>
+    <h1>Hello World</h1>
+  </WrapperContainer>
+);
+
+const WrapperContainer = styled.div`
+  color: red;
+`;
+
+export default IndexPage;
